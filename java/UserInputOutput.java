bed3c979c963688f8b14032de1fa703f233fb340
anushka2905 <anushkashrivastava29@gmail.com>
2024-07-08 20:08:07 +0530

    new file created

---
+++ java/UserInputOutput.java
@@ -0,0 +1,12 @@
+// for taking user input we will use library class (scanner)
+// class scanner which is coming from pakage 
+// java.util
+// to add this class in our program we will use input statement
+// import statements are placed above class 
+// declaration
+import java.util.Scanner; 
+class UserInputOutput {
+    public static void main(String[] args){
+        int number1;
+    }
+}
