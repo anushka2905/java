9301dda4031339e45972bfb45b7a03d6bcd7bba2
anushka2905 <anushkashrivastava29@gmail.com>
2024-07-09 10:40:26 +0530

    modified files

--- java/StudentDetails.java
+++ java/StudentDetails.java
@@ -5,13 +5,16 @@ class StudentDetails {
         long mobileNumber;
         String address;
         String email;
+        String programmingLanguages;
         name = "Anushka Shrivastava";
         mobileNumber = 9087536853L;
         address = "E3/343 arera colony bhopal";
         email = "anushkashrivastava29@gmail.com";
+        programmingLanguages = "C++,java,python,html";
         System.out.println("Name = " + name);
         System.out.println("MobileNumber = " + mobileNumber);
         System.out.println("Address = " + address);
         System.out.println("Email = " + email);
+        System.out.println("ProgrammingLanguage = " + programmingLanguages);
     } 
 }
