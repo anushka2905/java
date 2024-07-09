7c78233dd9b02699561565e3e8b10529b239ee62
anushka2905 <anushkashrivastava29@gmail.com>
2024-07-09 10:36:03 +0530

    new file created

--- java/StudentDetails.java
+++ java/StudentDetails.java
@@ -1,10 +1,17 @@
 // WAP to print your details like name, mobile no, address, email and known programming languages.
 class StudentDetails {
     public static void main(String[] args){
-        char name;
+        String name;
         long mobileNumber;
         String address;
         String email;
-        
-    }
+        name = "Anushka Shrivastava";
+        mobileNumber = 9087536853L;
+        address = "E3/343 arera colony bhopal";
+        email = "anushkashrivastava29@gmail.com";
+        System.out.println("Name = " + name);
+        System.out.println("MobileNumber = " + mobileNumber);
+        System.out.println("Address = " + address);
+        System.out.println("Email = " + email);
+    } 
 }
