79e4b62ceb57105240de7618a0243d0945a677e4
anushka2905 <anushkashrivastava29@gmail.com>
2024-07-11 20:23:07 +0530

    modified file

--- java/ArithematicOperator.java
+++ java/ArithematicOperator.java
@@ -14,12 +14,15 @@
 
             double temp = 9.9;
             temp++;
-            char tempChar = 'A';
-            tempChar++;
+            char tempChar = '0';
+            //tempChar++;
+            int smallA = 97;
+
             System.out.println("Num = " + num);
             System.out.println("Num 2 = " + num2);
             System.out.println("Temp = " + temp);
-            System.out.println("Temp char = " + tempChar);
+            System.out.println("Temp char = " + (int)tempChar);
+            System.out.println("smallA = " + (char)smallA);
 
         }
     
