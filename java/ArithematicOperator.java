0228824bf2ea8e3aa8923383f99e4512dff3e9d3
anushka2905 <anushkashrivastava29@gmail.com>
2024-07-11 20:12:18 +0530

    modified file

--- java/ArithematicOperator.java
+++ java/ArithematicOperator.java
@@ -10,7 +10,15 @@
         public static void main(String[] args){
             int num = 5;
             num++;
+            int num2 = num;
+
+            double temp = 9.9;
+            temp++;
+
             System.out.println("Num = " + num);
+            System.out.println("Num 2 = " + num2);
+            System.out.println("Temp = " + temp);
+
         }
     
  }
