20ad64865d3553043f7c66b10ceb0bd23e25dcc8
anushka2905 <anushkashrivastava29@gmail.com>
2024-07-11 19:58:59 +0530

    modified file

---
+++ java/ArithematicOperator.java
@@ -0,0 +1,16 @@
+//++ -> increment
+// -- -> decrement 
+// num++;
+// ++num; prefix
+// --num; prefix a++ + ++a
+ /**
+  * ArithematicOperator
+  */
+ class ArithematicOperator {
+        public static void main(String[] args){
+            int num = 5;
+            num++;
+            System.out.println("Num = " + num);
+        }
+    
+ }
