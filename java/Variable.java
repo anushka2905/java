d3d790c38b20024e511dfbdab61ad58742bbdb6a
anushka2905 <anushkashrivastava29@gmail.com>
2024-07-12 21:40:39 +0530

    modified files

--- java/Variable.java
+++ java/Variable.java
@@ -1,6 +1,22 @@
 // WAP to declare and initialize all the primittive type of variables and print the result on the console.
 class Variable {
     public static void main(String[] arga){
-        
+        //declare variable
+        // float or double
+        double marks;
+        int age;
+        char section;
+        // initialize     
+        // identifier = value or literal
+        marks = 40.5;
+        age = 40;
+        section = 'C';
+        // declare and initialize an string variable
+        String name = "Anushka Shrivastava";
+        // printing variables values of console/output 
+        System.out.println("Marks = " + marks);
+        System.out.println("Age = " + age);
+        System.out.println("Section = " + section);
+        System.out.println("Name = " + name);
     }
 }
