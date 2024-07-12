b53a2758a62a8654ef886229e1234baea1454db4
anushka2905 <anushkashrivastava29@gmail.com>
2024-07-12 21:39:12 +0530

    new file creeated

---
+++ java/Variable.java
@@ -0,0 +1,6 @@
+// WAP to declare and initialize all the primittive type of variables and print the result on the console.
+class Variable {
+    public static void main(String[] arga){
+        
+    }
+}
