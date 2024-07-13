3c0127063d02cd7377608ed6a656e825f6b0c258
anushka2905 <anushkashrivastava29@gmail.com>
2024-07-13 20:20:53 +0530

    new file created

--- if/DemoIfStatement.java
+++ if/DemoIfStatement.java
@@ -1,7 +1,12 @@
-
 import java.util.scanner;
-class DemoIfStatement {
+class DemoIfStatement{
     public static void main(String[] args){
-        
+        Scanner scanner = new Scanner(System.in);
+        int withdrawlAmount = scanner.nextInt();
+        int balance = 5000;
+        if(withdrawalAmount <= balance){
+            System.out.printl("Withdrawal permitted");
+        }
+        System.out.println("tysm");
     }
 }
