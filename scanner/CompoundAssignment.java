30f44146cdef3cb1db7fcb1baac4ed797422add2
anushka2905 <anushkashrivastava29@gmail.com>
2024-07-14 10:27:14 +0530

    new files

---
+++ scanner/CompoundAssignment.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class CompoundAssignment {
+    
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int number = 0;
+        number +=10;
+        System.out.println(number);
+    }
+    //compound assignment
+    /*
+    int a = 10;
+    a += 3; a = a + 3; 13
+    a -= 3; a = a - 3; 11
+    a *= 3; a = a * 3; 55
+    a /= 3; a = a / 3; 11
+    a %= 3; a = a % 3; 1
+    */
+
+}
+
+
