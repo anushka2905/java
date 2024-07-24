f3a54b75ef3e2f884c115a5aed8b41bd387a1360
anushka2905 <anushkashrivastava29@gmail.com>
2024-07-24 21:04:03 +0530

    files created

---
+++ operators/Temp.java
@@ -0,0 +1,9 @@
+class Temp{
+    public static void main(String[] args){
+        int max = Integer.MAX_VALUE;// Integer is class, MAX_VALUE constant
+        int min = Integer.MIN_VALUE;// Integer is class, MIN_VALUE constant
+        System.out.println("min = " + min);
+        System.out.println("max = " + max);
+        System.out.println("max + 1 = " + (max + max)); 
+    } 
+}
