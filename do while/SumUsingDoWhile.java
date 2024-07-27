dea20a63ab897322e1558735841b4e245a062e6d
anushka2905 <anushkashrivastava29@gmail.com>
2024-07-27 20:04:45 +0530

    files created

--- do while/SumUsingDoWhile.java
+++ do while/SumUsingDoWhile.java
@@ -5,10 +5,22 @@ public class SumUsingDoWhile {
         Scanner scanner = new Scanner(System.in);
         int sum = 0;
         int userInput;
+        int count = 0;
+        int userInputCounter = 0;
         do{
             System.out.println("enter an integer, enter 0 to stop");
             userInput = scanner.nextInt();
+            userInputCounter ++ ;
+            if (userInputCounter == 0)
             sum = sum + userInput;
-            }while (userInput != 0);
+            if(userInput <= 100){
+            }
+            else if(userInput > 100){
+                count ++;
+                 }
+            }
+            while (userInput != 0);
+            System.out.println("sum = " + sum);
+
     }
 }
---
+++ java/ArithmeticOperator.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class ArithmeticOperator {
+    // WAP to accept circumference of a circle from the user and print radius of circle
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter circumference of circle:");
+        double circumference = scanner.nextDouble();
+        double radius;
+        radius = (circumference/2*3.14);
+
+        System.out.println("Radius of circle : "+radius);
+
+
+
+    }
+}
+
