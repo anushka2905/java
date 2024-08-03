6bda131eccc71c499290c8136d84dd65b90f6d70
anushka2905 <anushkashrivastava29@gmail.com>
2024-08-03 20:11:41 +0530

    file added

---
+++ practice-Questions/EnterStudentResult.java
@@ -0,0 +1,17 @@
+// WAP that promts the user to enter the no. of students & each student name and score and finally display name of name of student with hightest marks
+import java.util.Scanner;
+public class EnterStudentResult {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter number of students: ");
+        int numberOfStudent = scanner.nextInt();
+        int nextScore = 0;
+        String maxScoreName = null;
+        
+        // conditional statement
+        
+
+
+    }
+
+}
