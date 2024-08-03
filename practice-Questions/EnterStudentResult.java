692ad1fcbe2bc24755e1294b2ce36cfbb11f7641
anushka2905 <anushkashrivastava29@gmail.com>
2024-08-03 23:34:50 +0530

    file added

--- practice-Questions/EnterStudentResult.java
+++ practice-Questions/EnterStudentResult.java
@@ -5,10 +5,25 @@ public class EnterStudentResult {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter number of students: ");
         int numberOfStudent = scanner.nextInt();
+        scanner.nextLine();
         int nextScore = 0;
+      
         String maxScoreName = null;
-        
-        // conditional statement
+
+        for(int i = 1 ; i <= numberOfStudent ; i++){
+            System.out.println("Enter number of students: ");
+            String name = scanner.nextLine();
+            System.out.println("Enter marks of student: ");
+            int score = scanner.nextInt();
+            int maxScore = scanner.nextInt();
+            if (score > maxScore){
+                maxScore = score;
+                maxScoreName = name;
+               
+            }
+        } 
+     System.out.println("maxScoreName = " + maxScoreName);
+
         
 
 
